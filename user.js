f3a71daf4ad264bf51195b7a2bc6455cb779da52
mark <liujie0420@126.com>
2015-11-28 16:47:56 +0800

    delete file user.js

--- user.js
+++
@@ -1,7 +0,0 @@
-var aes=require('./util/aes');
-var data="{'imei':'中国==','mac':'MTI6MzQ6MTI6MjM6NDI6MTI=','version':'MS4w'}";
-var encrypt=aes.encrypt(data);
-console.log('plain text is: '+data);
-console.log('encrypt text is: '+encrypt);
-var decrypt=aes.decrypt(encrypt);
-console.log('解密后: '+decrypt);
